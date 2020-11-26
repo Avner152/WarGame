1da8a2ac7c6dd5d4ea1452ca6bbc99853ab3aec9
Avner152 <Avnerx92@gmail.com>
2020-11-27 00:10:59 +0200

    - Improved Code for printing text. - Better View for Instructions_Activity - english mistakes correction.

--- app/src/main/java/com/example/avners/Instructions_Activity.java
+++ app/src/main/java/com/example/avners/Instructions_Activity.java
@@ -10,39 +10,21 @@ import android.widget.TextView;
 public class Instructions_Activity extends AppCompatActivity {
 
     private Handler handler = new Handler();
-    private String[] strs = new String[9];
-    private String newStr = "";
+    private String newStr = "", str;
     private static final int DELAY = 40;
     private TextView tv;
-    private char[][] ch = new char[9][92];
-    private int row = 0, col = 0, countForSpace = 0;
-
+    private int n = 0;
 
     Runnable r = new Runnable() {
 
         @Override
         public void run() {
-            try {
-                newStr += ch[row][col++];
+                newStr += str.charAt(n++);
                 tv.setText(newStr);
-                if(col == strs[row].length()){
-                    newStr+= "\n";
-                    row++;
-                    col = 0;
-                }
-                if (row < 2)
-                    handler.postDelayed(r, 80);
-                else if(row <= 9 )
-                    handler.postDelayed(r, 40);
-
-                else{
-                    stopDelay();
-                }
-            }
-            catch (Exception e){
-
-            }
-
+                if(n < 71)
+                    handler.postDelayed(r, 50);
+                else if(n < str.length())
+                    handler.postDelayed(r, 25);
         }
     };
 
@@ -51,18 +33,20 @@ public class Instructions_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions_);
         tv = findViewById(R.id.R_id_TEXT);
-        printStr();
-        int num = findMyLength();
+        printMyStr();
         startDelay();
-
-
     }
 
-    private int findMyLength() {
-        int sum = 0;
-        for (int i = 0; i < 9; i ++)
-            sum += strs[i].length();
-        return sum;
+    private void printMyStr() {
+     str = "Greeting, Summoners!\n" +
+            "Welcome to our War! So here are some rules:\n\n" +
+            "1) The game starts when both participant's score initiated with Zero.\n\n" +
+            "2) In Order to take the next step, the user has to click 'DEAL'.\n\n" +
+            "3) The participant with the greater power level gains +1 to his score! the other stays the same.\n\n" +
+            "4) When it's a tie then both participants losing 1 point.\n\n" +
+            "5) When it's a tie and any participant has Zero points then they stay the same.\n\n" +
+            "6) The first participant that gets to Ten points is the Winner! Congrants !!\n\n\n" +
+            "And Most Important --> Have Fun!!!";
     }
 
 
@@ -76,33 +60,8 @@ public class Instructions_Activity extends AppCompatActivity {
     }
 
 
-    private void printStr() {
-        strs[0] = "Greeting, Summoners!";
-        strs[1] = "Welcome the our War! so here's some rules:";
-        strs[2] = "1) The game starts when both participant's score initiated with 0.";
-        strs[3] = "2) In Order to take the next step, the user has to click 'DEAL'.";
-        strs[4] = "3) The participant with the greater power level wins +1 score! the other stays the same.";
-        strs[5] = "4) When it's a tie then both participants losing 1 point.";
-        strs[6] = "5) When it's a tie and any participant has 0 points then they stay the same.";
-        strs[7] = "6) The first participant that gets to 10 points is the Winner! Congrants !!";
-        strs[8] = "HAVE FUN!";
-
-        for (int i = 0; i < 9; i++)
-            strToChar(strs[i], i);
-    }
-
-
 
     private void stopDelay() {
         handler.removeCallbacks(r);
-
-    }
-
-    private void strToChar(String str, int index) {
-        for (int i = 0; i < str.length(); i++) {
-            if ((str.charAt(i)+"").isEmpty())
-                ch[index][i] = ' ';
-            ch[index][i] = str.charAt(i);
-        }
     }
 }
