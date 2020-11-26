9b5b506f06add07cf11aa0eb5dacf36c38d0a7b7
Avner152 <Avnerx92@gmail.com>
2020-11-26 23:37:46 +0200

    Instruction Activity has been updated. - now typing chars in the Instructions_Activity with a delay. - game is no longer manual. from this update its automated with timer up to 10 wins.

--- app/src/main/java/com/example/avners/Activity_Game.java
+++ app/src/main/java/com/example/avners/Activity_Game.java
@@ -3,6 +3,7 @@ package com.example.avners;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,6 +14,8 @@ import java.util.ArrayList;
 public class Activity_Game extends AppCompatActivity {
 
     private static ArrayList<Card> cards = new ArrayList<>();
+
+    final int DELAY = 1000;
     private TextView game_LBL_Num1, game_LBL_Num2, game_LBL_Answer, game_LBL_Winner;
     private ImageView game_IMG_LeftCard, game_IMG_RightCard;
     private Button game_BTN_deal, game_BTN_forfeit;
@@ -25,8 +28,13 @@ public class Activity_Game extends AppCompatActivity {
         setContentView(R.layout.activity__game);
         findViews();
         createCards();
+        startGame();
         initViews();
 
+    }
+
+    private void startGame() {
+    handler.postDelayed(r,DELAY);
     }
 
     private void createCards() {
@@ -56,18 +64,20 @@ public class Activity_Game extends AppCompatActivity {
         cards.add(new Card(23,5, "Pilaf"));
     }
 
-    private void initViews() {
-        game_BTN_deal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                game_LBL_Winner.setText("The Winner is:");
-               if (score1 == 10 || score2 == 10 )
-                   finish();
-               else
-                 shuffle();
-            }
-        });
 
+
+    Handler handler = new Handler();
+    private  Runnable r = new Runnable() {
+        @Override
+        public void run() {
+            handler.postDelayed(this, DELAY);
+            int res = shuffle();
+            if (res != 0)
+                stopGame(res);
+        }
+    };
+
+    private void initViews() {
         game_BTN_forfeit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -79,7 +89,6 @@ public class Activity_Game extends AppCompatActivity {
     private void findViews() {
         game_LBL_Winner = findViewById(R.id.game_LBL_Winner);
         game_BTN_forfeit = findViewById(R.id.game_BTN_forfeit);
-        game_BTN_deal = findViewById(R.id.game_BTN_deal);
         game_LBL_Num1 = findViewById(R.id.game_LBL_NUM1);
         game_LBL_Num2 = findViewById(R.id.game_LBL_NUM2);
         game_LBL_Answer = findViewById(R.id.game_LBL_Answer);
@@ -87,7 +96,12 @@ public class Activity_Game extends AppCompatActivity {
         game_IMG_RightCard = findViewById(R.id.game_IMG_RightCard);
     }
 
-    private void shuffle(){
+    private void stopGame(int res) {
+        game_LBL_Answer.setText("The Winner is: Player #" + res);
+        handler.removeCallbacks(r);
+    }
+
+    private int shuffle(){
         num1 = (int) (1 + Math.random() * 23) -1;
         num2 = (int) (1 + Math.random() * 23) -1;
 
@@ -110,12 +124,18 @@ public class Activity_Game extends AppCompatActivity {
                     score1--;
                 if(score2 > 0)
                     score2--;
-
             }
+
+
         game_LBL_Num1.setText("" + score1);
         game_LBL_Num2.setText("" + score2);
 
+        if (score1 == 10)
+            return 1;
+        else if (score2 == 10)
+            return 2;
 
+        return 0;
     }
 
         private int findPowerById(int id) {
@@ -132,5 +152,4 @@ public class Activity_Game extends AppCompatActivity {
             int rightCard = getResources().getIdentifier("img" + num2, "drawable", getPackageName() );
             game_IMG_RightCard.setImageResource(rightCard);
     }
-
 }
--- app/src/main/java/com/example/avners/Instructions_Activity.java
+++ app/src/main/java/com/example/avners/Instructions_Activity.java
@@ -1,14 +1,108 @@
 package com.example.avners;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
 
 public class Instructions_Activity extends AppCompatActivity {
+
+    private Handler handler = new Handler();
+    private String[] strs = new String[9];
+    private String newStr = "";
+    private static final int DELAY = 40;
+    private TextView tv;
+    private char[][] ch = new char[9][92];
+    private int row = 0, col = 0, countForSpace = 0;
+
+
+    Runnable r = new Runnable() {
+
+        @Override
+        public void run() {
+            try {
+                newStr += ch[row][col++];
+                tv.setText(newStr);
+                if(col == strs[row].length()){
+                    newStr+= "\n";
+                    row++;
+                    col = 0;
+                }
+                if (row < 2)
+                    handler.postDelayed(r, 80);
+                else if(row <= 9 )
+                    handler.postDelayed(r, 40);
+
+                else{
+                    stopDelay();
+                }
+            }
+            catch (Exception e){
+
+            }
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions_);
+        tv = findViewById(R.id.R_id_TEXT);
+        printStr();
+        int num = findMyLength();
+        startDelay();
+
+
+    }
+
+    private int findMyLength() {
+        int sum = 0;
+        for (int i = 0; i < 9; i ++)
+            sum += strs[i].length();
+        return sum;
+    }
+
+
+    private void startDelay() {
+        handler.postDelayed(r, DELAY);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+
+    private void printStr() {
+        strs[0] = "Greeting, Summoners!";
+        strs[1] = "Welcome the our War! so here's some rules:";
+        strs[2] = "1) The game starts when both participant's score initiated with 0.";
+        strs[3] = "2) In Order to take the next step, the user has to click 'DEAL'.";
+        strs[4] = "3) The participant with the greater power level wins +1 score! the other stays the same.";
+        strs[5] = "4) When it's a tie then both participants losing 1 point.";
+        strs[6] = "5) When it's a tie and any participant has 0 points then they stay the same.";
+        strs[7] = "6) The first participant that gets to 10 points is the Winner! Congrants !!";
+        strs[8] = "HAVE FUN!";
+
+        for (int i = 0; i < 9; i++)
+            strToChar(strs[i], i);
+    }
+
+
+
+    private void stopDelay() {
+        handler.removeCallbacks(r);
+
+    }
+
+    private void strToChar(String str, int index) {
+        for (int i = 0; i < str.length(); i++) {
+            if ((str.charAt(i)+"").isEmpty())
+                ch[index][i] = ' ';
+            ch[index][i] = str.charAt(i);
+        }
     }
 }
