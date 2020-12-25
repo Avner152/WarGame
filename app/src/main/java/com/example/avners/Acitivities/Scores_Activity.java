a24028b864545d66a5ef903c21cba32b97965bf3
Avner152 <Avnerx92@gmail.com>
2020-12-26 00:55:12 +0200

    - map fragment is ready. - google map key is set. all that remains is to get from list callback and then put in the map.

--- app/src/main/java/com/example/avners/Acitivities/Scores_Activity.java
+++ app/src/main/java/com/example/avners/Acitivities/Scores_Activity.java
@@ -17,28 +17,25 @@ import java.util.ArrayList;
 
 public class Scores_Activity extends Main implements Fragment_list.CallBack_list {
 
-    protected static TextView TXT_score1, TXT_score2, LBL_Total_Wins1, LBL_Total_Wins2;
-    private ImageView score_IMG_wp;
+    protected static TextView LBL_Total_Wins1, LBL_Total_Wins2;
+//    private ImageView score_IMG_wp;
     private Fragment_list fragment_list = new Fragment_list();
-
     private Fragment_map fragment_map;
     private ListView listView;
     private ArrayList<String> myList = new ArrayList<>();
-
+    private String str;
+    private double d1, d2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scores);
         findViews();
-        String str = "abc";
-
 //        Glide.with(this).load(R.drawable.wpnew).into(score_IMG_wp);
         // get the data from the previous intend
         // create new bundle
         // link bundle to fragment
         Bundle bundle = new Bundle();
-
         fragment_list.setArguments(bundle);
 
         listFragManager();
@@ -46,60 +43,24 @@ public class Scores_Activity extends Main implements Fragment_list.CallBack_list
         //  MAP //
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
-        ft.add(R.id.score_LAY_list, fragment_list).commit();
+        ft.replace(R.id.score_LAY_list, fragment_list).commit();
 
         fragment_map = new Fragment_map();
         getSupportFragmentManager().beginTransaction().add(R.id.score_LAY_map, fragment_map).commit();
-
-//        technicalThingsToDo();
-//        printWins();
-
     }
 
     private void listFragManager() {
-
         fragment_list.setCallBackList(this);
-
     }
 
-    private void technicalThingsToDo() {
-        LBL_Total_Wins1.setText(countWins1 + "");
-        LBL_Total_Wins2.setText(countWins2 + "");
-    }
 
 
     private void findViews() {
         listView = findViewById(R.id.score_list);
-//        score_IMG_wp = findViewById(R.id.score_IMG_wp);
     }
     @Override
     protected void onStart() {
         super.onStart();
-
-
-    }
-
-    protected  void printWins() {
-        Log.d("pttt", "list1 size: " + l1.size() + " | list2 size " + l2.size());
-        Log.d("pttt", "countWins1: " + countWins1 + " | countWins2: " + countWins2);
-    if(countWins1 + countWins2 == 10) {
-        stScore = "";
-        ndScore = "";
-    }
-
-        for (int i = 0; i < l1.size(); i++) {
-            Log.d("ptt", "printWins: " + l1.size() + ", " + l1.get(i));
-                stScore += l1.get(i) + "\n\n";
-                ndScore += l2.get(i) + "\n\n";
-            }
-            stScore = TXT_score1.getText() + stScore;
-            ndScore = TXT_score2.getText() + ndScore;
-            l1.clear();
-            l2.clear();
-
-        TXT_score1.setText(stScore + "\n");
-        TXT_score2.setText(ndScore + "\n");
-
     }
 
     @Override
@@ -126,7 +87,6 @@ public class Scores_Activity extends Main implements Fragment_list.CallBack_list
 
     @Override
     public void onScoreSampleListener(double d1, double d2) {
-        Log.i("yoni: ", "onScoreSampleListener: " + d1 + " , " + d2);
         fragment_map.setCoord(d1, d2);
     }
 
--- app/src/main/java/com/example/avners/fragments/Fragment_map.java
+++ app/src/main/java/com/example/avners/fragments/Fragment_map.java
@@ -1,5 +1,6 @@
 package com.example.avners.fragments;
 
+import android.graphics.Camera;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,22 +12,60 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.avners.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Fragment_map extends Fragment {
+public class Fragment_map extends Fragment{
 
     private TextView tv_title;
     private View v;
+    private double lant, lont;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.map_fragment, container, false);
+
+        Bundle b = new Bundle();
+        lant = b.getDouble("lant",18);
+        lont = b.getDouble("lont", 12);
+        // init my map //
+        SupportMapFragment supportMapFragment = (SupportMapFragment)
+                getChildFragmentManager().findFragmentById(R.id.google_Map);
+
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(LatLng latLng) {
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        // sets position of marker //
+                        markerOptions.position(latLng);
+                        // set title of marker //
+                        markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+                        // remove all markers //
+                        googleMap.clear();
+                        // Animating to zoom the marker //
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+
+                        // add marker on map //
+                        googleMap.addMarker(markerOptions);
+
+                    }
+                });
+            }
+        });
         return v;
     }
 
     public void setCoord(double d1, double d2){
-       tv_title =  v.findViewById(R.id.title_TXT);
-       tv_title.setText(d1 + " , " +  d2);
+        tv_title =  v.findViewById(R.id.title_TXT);
+        tv_title.setText(d1 + " , " +  d2);
     }
 
 }
